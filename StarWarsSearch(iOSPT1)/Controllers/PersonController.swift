439b640809a2d0d69847581891a3fcfe0f95d930
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-23 22:37:17 -0700

    finised guided project

---
+++ StarWarsSearch(iOSPT1)/Controllers/PersonController.swift
@@ -0,0 +1,9 @@
+C//
+//  PersonController.swift
+//  StarWarsSearch(iOSPT1)
+//
+//  Created by Dongwoo Pae on 5/23/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
---
+++ StarWarsSearch(iOSPT1)/Views/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  StarWarsSearch(iOSPT1)
+//
+//  Created by Dongwoo Pae on 5/23/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
